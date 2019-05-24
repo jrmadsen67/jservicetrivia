52f78fe2e6b733c87e01ad2f7ef134bd746d1fb6
Jeff Madsen <jrmadsen67@gmail.com>
2019-05-24 08:12:55 +0000

    one paltry test with a hope for more

--- tests/Feature/MakeGameTest.php
+++ tests/Feature/MakeGameTest.php
@@ -8,10 +8,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class MakeGameTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function user_can_create_new_game(){
         $response = $this->post(route('games.game.store'), []);
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
     }
 }
